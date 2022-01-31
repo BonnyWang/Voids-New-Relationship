bd4b87e4a97a269ed501b1d1bbb7a967d40ca604
BonnyWang <bonny.y.gardy@gmail.com>
2022-01-30 20:12:52 -0500

    plot by bins accomplished

--- plotRelationship.py
+++ plotRelationship.py
@@ -1,4 +1,6 @@
-import string;
+from operator import index
+import string
+from xml.dom import IndexSizeErr;
 import numpy as np;
 import matplotlib.pyplot as plt;
 
@@ -10,12 +12,19 @@ R_max = 0;
 R_min = 0;
 
 bin_Number = 10;
+binColumn = 4;
+# 8 is the density contrast and 
+statisticsColumn = 13;
+
 
 
 def fileProcess():
     fd = open(fileIn);
+    
     lines = fd.readlines();
-    lines = lines[50:1000];
+    
+    lines = lines[1:];
+    
     return lines;
 
 def findMaxValue(lines, Column):
@@ -37,6 +46,27 @@ def divdeIntoBins(bin_Number, max, min = 0):
     
     return bins;
 
+def calculateBinStatistics(lines, bins, binColumn, statisticColumn):
+
+    statisticsEachBin = np.zeros(bins.size);
+    statisticsNumber = np.zeros(bins.size);
+
+    for line in lines:
+        datas = np.array(line.split(" ")).astype(np.float);
+        for index in range(bins.size):
+            if (datas[binColumn] < bins[index]) and (datas[binColumn] > (bins[index-1] if index-1 >= 0 else 0)):
+                # Data belone to curent bin 
+                statisticsEachBin[index] += datas[statisticColumn];
+                statisticsNumber[index] += 1;
+    
+    
+    # Normalize
+    for index in range(statisticsEachBin.size):
+        statisticsEachBin[index] = statisticsEachBin[index]/statisticsNumber[index]
+    
+    return statisticsEachBin;
+            
+
 
 def plotRelationship(lines,column1,column2):
     x = [];
@@ -54,10 +84,22 @@ def plotRelationship(lines,column1,column2):
     plt.show();
 
 if __name__ == "__main__":
+    
+    
     lines = fileProcess();
-
-    R_max = findMaxValue(lines, 4);
-    print(divdeIntoBins(bin_Number,R_max))
+    
+    R_max = findMaxValue(lines, binColumn);
+    bins = divdeIntoBins(bin_Number,R_max);
+    
+    statisticBins = calculateBinStatistics(lines,bins,binColumn,statisticsColumn);
+    
+    print(bins);
+    print(statisticBins);
+    
+    print(findMaxValue(lines, statisticsColumn))
+    
+    plt.scatter(bins, statisticBins);
+    plt.show();
 
     # plotRelationship(lines, 4,8);
 
