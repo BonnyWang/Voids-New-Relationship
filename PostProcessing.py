59a9dfb847f5dc9202505d51e6c232d744275fa6
BonnyWang <bonny.y.gardy@gmail.com>
2022-04-12 14:19:48 -0400

    change method

--- PostProcessing.py
+++ PostProcessing.py
@@ -1,10 +1,23 @@
+from operator import length_hint
 from scipy.stats import chisquare
 import pandas as pd;
+import numpy as np;
 
 # Chi value test
 
 def getChiValue():
-    print(chisquare(producedValue, trueValue));
     
+    data = pd.read_csv("./Data/borrarEllipAll.txt", sep=" ", header=None);
+    
+    omegaM_True = data.iloc[:,0].to_numpy().ravel();    
+    omegaM__Predicted = data.iloc[:,5].to_numpy().ravel();
+    
+    # print(len(omegaM_True));    
+    # print(len(omegaM__Predicted));    
+    
+    # Not working
+    # chi, p = chisquare(omegaM__Predicted, omegaM_True);
+
+
 if __name__ == "__main__":
     getChiValue();
